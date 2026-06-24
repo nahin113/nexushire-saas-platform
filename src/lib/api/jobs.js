2f6bd693f466bb510c2a80a4f974ee1b9d0b502a
nahin113 <nahinahmed113@gmail.com>
2026-06-24 23:51:45 +0600

    implemented pagination so that website dont load thousands of users data at once and implemented search, filter functionalities from the backend to keep the web application light

--- src/lib/api/jobs.js
+++ src/lib/api/jobs.js
@@ -9,8 +9,8 @@ export const getCompanyJobs = async (companyId, status = "active") => {
   return res.json();
 };
 
-export const getJobs = async () => {
-  return serverFetch("api/jobs");
+export const getJobs = async (queryString) => {
+  return serverFetch(`api/jobs?${queryString}`);
 };
 
 export const getJobById = async (JobId) => {
